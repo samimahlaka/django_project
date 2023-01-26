019e33aebfc5fd84895f4b2890dcaa55c230a5c1
samimahlaka <samimahlaka@gmail.com>
2023-01-25 23:48:47 -0600

    Update User-Profile

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path , include
 from users import views as users_views
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,4 @@ urlpatterns = [
     path('profile/', users_views.profile , name  = 'profile'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout')   
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
