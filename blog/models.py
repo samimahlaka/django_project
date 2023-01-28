b25b76b46beafb40f2e8bf41db77ff68fe1b714e
samimahlaka <samimahlaka@gmail.com>
2023-01-27 23:37:01 -0600

    Post Create View

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User;
 from django.utils import timezone;
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100);
@@ -10,5 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title;
+
+   
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
-
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView , DetailView, CreateView
 
 context = {
-    'posts_key': Post.objects.all()
+    'posts': Post.objects.all()
 }
 def home(request):
     return render(request, 'blog/home.html',context);
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/home.html'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post;
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post;
+    fields = ['title', 'content']
+    success_url = reverse_lazy('blog-home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title' : 'BlogAbout'});
