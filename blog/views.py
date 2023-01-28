c9af0c0e14a1f7865d72f049f7cf10786173de73
samimahlaka <samimahlaka@gmail.com>
2023-01-28 00:37:41 -0600

    Post Update View

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from blog.models import Post
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView , DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ( ListView , 
+DetailView, 
+CreateView,
+UpdateView
+)
+from django.contrib import messages
 
 context = {
     'posts': Post.objects.all()
@@ -29,8 +34,23 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post;
+    fields = ['title', 'content'];
+    success_url = reverse_lazy('blog-home')
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user;
+        messages.success(self.request,f'Post has been updated')
+        return super().form_valid(form)
 
-
+    def test_func(self):
+        post = self.get_object();
+        if self.request.user == post.author:
+            return True;
+        else:
+            return False
+        
 def about(request):
     return render(request, 'blog/about.html', {'title' : 'BlogAbout'});
