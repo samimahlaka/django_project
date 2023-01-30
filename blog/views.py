cbc2d07b3debd0e60824154f205009c493f531bc
samimahlaka <samimahlaka@gmail.com>
2023-01-30 16:15:46 -0600

    Pagination :  Previous Page

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,7 @@ class PostListView(ListView):
     context_object_name = 'posts'
     template_name = 'blog/home.html'
     ordering = ['-date_posted']
+    paginate_by = 3;
 
 class PostDetailView(DetailView):
     model = Post;
