154a05a39105d17ebcf0b3a3009af1ab35c9f6cc
samimahlaka <samimahlaka@gmail.com>
2023-02-02 23:26:17 -0600

    Deploy Initial Commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -144,8 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'samimahlaka@gmail.com'
-EMAIL_HOST_PASSWORD = 'jgiqnlcghuaglmub'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
